816cf374a31c27b83572de500dfbaf7b1f44c305
Jodaías <jodaias2013@gmail.com>
2020-08-18 20:40:27 -0300

    arquivos de configuração

---
+++ Contato.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace website
+{
+    public class Contato
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+
+        public override string ToString()
+        {
+            return Nome;
+        }
+    }
+}
